7b7c8e197cc50a7960139098f5dcaa5b4a7860b8
HoveWisdom <hovewisdom@gmail.com>
2026-04-08 15:22:29 +0200

    Invoice services

--- InvoiceService/Data/InvoiceDbContext.cs
+++ InvoiceService/Data/InvoiceDbContext.cs
@@ -1,12 +1,70 @@
 using Microsoft.EntityFrameworkCore;
-using InvoiceService.Models;
+using Shared.Entities;
 
-namespace InvoiceService.Data
+namespace InvoiceService.Data;
+
+public class InvoiceDbContext : DbContext
 {
-    public class InvoiceDbContext : DbContext
+    public InvoiceDbContext(DbContextOptions<InvoiceDbContext> options) 
+        : base(options)
     {
-        public InvoiceDbContext(DbContextOptions<InvoiceDbContext> options) : base(options) { }
+    }
 
-        public DbSet<Invoice> Invoices { get; set; }
+    public DbSet<Consultant> Consultants { get; set; } = null!;
+    public DbSet<Invoice> Invoices { get; set; } = null!;
+    public DbSet<Approval> Approvals { get; set; } = null!;
+    public DbSet<Payment> Payments { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure Consultant
+        modelBuilder.Entity<Consultant>(entity =>
+        {
+            entity.HasKey(c => c.ConsultantId);
+            entity.Property(c => c.Name).IsRequired().HasMaxLength(200);
+            entity.Property(c => c.Email).IsRequired().HasMaxLength(200);
+            entity.HasIndex(c => c.Email).IsUnique();
+        });
+
+        // Configure Invoice
+        modelBuilder.Entity<Invoice>(entity =>
+        {
+            entity.HasKey(i => i.InvoiceId);
+            entity.Property(i => i.Amount).HasColumnType("decimal(18,2)");
+            entity.Property(i => i.Status).IsRequired().HasMaxLength(50);
+            
+            entity.HasOne(i => i.Consultant)
+                  .WithMany(c => c.Invoices)
+                  .HasForeignKey(i => i.ConsultantId)
+                  .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        // Configure Approval
+        modelBuilder.Entity<Approval>(entity =>
+        {
+            entity.HasKey(a => a.ApprovalId);
+            entity.Property(a => a.Approver).IsRequired().HasMaxLength(200);
+            entity.Property(a => a.Status).IsRequired().HasMaxLength(50);
+            
+            entity.HasOne(a => a.Invoice)
+                  .WithMany(i => i.Approvals)
+                  .HasForeignKey(a => a.InvoiceId)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        // Configure Payment
+        modelBuilder.Entity<Payment>(entity =>
+        {
+            entity.HasKey(p => p.PaymentId);
+            entity.Property(p => p.Amount).HasColumnType("decimal(18,2)");
+            entity.Property(p => p.Status).IsRequired().HasMaxLength(50);
+            
+            entity.HasOne(p => p.Invoice)
+                  .WithOne(i => i.Payment)
+                  .HasForeignKey<Payment>(p => p.InvoiceId)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
     }
 }
